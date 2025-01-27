abaf65e87dd496631bac58c51f0b4cb5c8178a93
elneko42 <franson177@gmail.com>
2025-01-27 17:24:45 +0100

    Update Entity Framework Core packages to version 9.0.1 and adjust project references

--- TodoList2.Server/obj/Debug/net8.0/TodoList2.Server.AssemblyInfo.cs
+++ TodoList2.Server/obj/Debug/net8.0/TodoList2.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoList2.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bdbc1a1d1e138183f03b54bdcfa8a196c4813577")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cda5c3ea1761b5dc15f0b3fb5beb1c812e946d4d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoList2.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoList2.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
